6c26e45f2f8dd4042939d89febc5c4176861594c
Anya Lin <hukk1996@gmail.com>
2023-10-12 11:03:13 +0800

    20231012

--- htdocs/luci-static/resources/view/netspeedtest/speedtest.js
+++ htdocs/luci-static/resources/view/netspeedtest/speedtest.js
@@ -103,7 +103,7 @@ return view.extend({
 				window.location = window.location.href.split('#')[0];
 			}, L.env.apply_display * 500);
 
-			return fs.exec('/etc/init.d/netspeedtest', ['speedtest'])
+			return fs.exec('/usr/lib/netspeedtest/speedtest')
 				.catch(function(e) { ui.addNotification(null, E('p', e.message), 'error') });
 		};
 
